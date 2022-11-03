cd109e70c5dea2ff2b216ae265cb8d74f2c1fc1c
CanerCagri <canerrcagrii@gmail.com>
2022-11-03 15:49:49 +0300

    Tableview created.

---
+++ ListApp/ViewControllers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Caner Çağrı on 2.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+    }
+    
+    func configureViewController() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
+
