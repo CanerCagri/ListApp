0da75feabf60a1f61da508ec2e2a332de9f85408
CanerCagri <canerrcagrii@gmail.com>
2022-11-03 16:35:51 +0300

    TableViewCell, swipe action added.

--- ListApp/ViewControllers/ViewController.swift
+++ ListApp/ViewControllers/ViewController.swift
@@ -19,18 +19,29 @@ class ViewController: UIViewController {
     }
     
     func configureViewController() {
+        title = "ListApp"
         tableView.delegate = self
         tableView.dataSource = self
     }
 
     
     @IBAction func leftBarButtonTapped(_ sender: UIBarButtonItem) {
-        data.removeAll()
-        tableView.reloadData()
+        let alertController = UIAlertController(title: "Deleting All Items", message: nil, preferredStyle: .alert)
+        
+        let deleteButton = UIAlertAction(title: "Delete", style: .default) { _ in
+            self.data.removeAll()
+            self.tableView.reloadData()
+        }
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(deleteButton)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true)
     }
     
     @IBAction func rightBarButtonTapped(_ sender: UIBarButtonItem) {
-        let alertController = UIAlertController(title: "Add New Element", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .alert)
         
         let defaultButton = UIAlertAction(title: "Ekle",
                                           style: .default) { _ in
@@ -61,6 +72,20 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = data[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .normal,
+                                              title: "Delete") { _, _, _ in
+            self.data.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+        
+        deleteAction.backgroundColor = .systemRed
+        
+        let config = UISwipeActionsConfiguration(actions: [deleteAction])
+        return config
     }
 
 }
