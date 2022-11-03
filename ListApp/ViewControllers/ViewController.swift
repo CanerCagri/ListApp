88fa9ad7257b54fe41f19e9c3965f0a3a5e978ee
CanerCagri <canerrcagrii@gmail.com>
2022-11-03 16:21:33 +0300

    Left and Right bar buttons created.

--- ListApp/ViewControllers/ViewController.swift
+++ ListApp/ViewControllers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var data = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +23,44 @@ class ViewController: UIViewController {
         tableView.dataSource = self
     }
 
+    
+    @IBAction func leftBarButtonTapped(_ sender: UIBarButtonItem) {
+        data.removeAll()
+        tableView.reloadData()
+    }
+    
+    @IBAction func rightBarButtonTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Add New Element", message: nil, preferredStyle: .alert)
+        
+        let defaultButton = UIAlertAction(title: "Ekle",
+                                          style: .default) { _ in
+            let textFieldText = alertController.textFields?.first?.text
+            if textFieldText != "" {
+                self.data.append((textFieldText)!)
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancelButton = UIAlertAction(title: "Vazgec", style: .cancel)
+        
+        alertController.addTextField()
+        alertController.addAction(defaultButton)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true)
+      
+    }
 
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
 }
